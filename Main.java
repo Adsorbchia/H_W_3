dc0ec6cad0e447ccf746aff9ce680294b6bfc229
Natalya <117934761+Adsorbchia@users.noreply.github.com>
2023-07-23 19:36:13 +0400

    add new JavaClass

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+        list.addNode(8);
+        list.addNode(9);
+        list.addNode(99);
+        list.revert();
+        list.printNode();
+
+
+
+
+        }
+    }
